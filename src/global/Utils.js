731de2c51837886881cd737660822405b094624a
Dreamweaver2k <chandlerault@gmail.com>
2021-05-10 16:34:32 -0600

    added damage, enemy despawn

--- src/global/Utils.js
+++ src/global/Utils.js
@@ -1,11 +1,14 @@
-import { Global } from './';
+import { Global } from 'global';
 import { Box3 } from 'three';
+import { Clock, Land, Banana, Room, Apple, Avocado, Dice} from 'objects';
+
 
 function intersectsEnemy(box) {
     if (Global.enemies == undefined) return undefined;
     for (const enemy of Global.enemies) {
         const enemyBox = new Box3().setFromObject(enemy);
         if (box.intersectsBox(enemyBox)) {
+            enemy.state.health -= Math.random() * 6;
             return enemy;
         }
     }
