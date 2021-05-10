96fa9ec389e4260e4b2dfb208988317fa9ab540d
Will Drury <wdrury@princeton.edu>
2021-05-09 17:41:01 -0700

    fixed dice bug

--- src/global/Global.js
+++ src/global/Global.js
@@ -6,10 +6,17 @@ export default {
     clock: null,
     enemies: [],
 
+    ///////////////////////////
     // Constants
+    ///////////////////////////
+
+    // Clock
     MOVEMENT_SPEED: .1,
     CLOCK_ROTATION_OFFSET: .75 * Math.PI,
     DISTANCE_TO_CAMERA: 10,
+
+    // Dice
     DICE_ROTATION_SPEED: Math.PI,
+    DICE_MOVEMENT_SPEED: .5,
 
 };
